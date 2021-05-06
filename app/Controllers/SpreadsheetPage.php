2b094e7b9eee32d6616e0df5bd7293f5f2de04b7
sedera-tax <sedera.aina@gmail.com>
2021-05-06 14:55:20 +0400

    excel custom

--- app/Controllers/SpreadsheetPage.php
+++ app/Controllers/SpreadsheetPage.php
@@ -3,6 +3,8 @@ namespace App\Controllers;
 
 use CodeIgniter\Controller;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Reader\Html;
 use PhpOffice\PhpSpreadsheet\IOFactory;
@@ -10,10 +12,38 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 class SpreadsheetPage extends Controller
 {
     protected Spreadsheet $spreadsheet;
+    protected array $persons;
 
     public function __construct()
     {
         $this->spreadsheet = new Spreadsheet();
+        $this->persons = [
+            [
+                'name' => 'Cristiano Ronaldo',
+                'age' => 35,
+                'category' => 'Football'
+            ],
+            [
+                'name' => 'Lebron James',
+                'age' => 36,
+                'category' => 'Basketball'
+            ],
+            [
+                'name' => 'Rafael Nadal',
+                'age' => 34,
+                'category' => 'Tennis'
+            ],
+            [
+                'name' => 'Usain Bolt',
+                'age' => 34,
+                'category' => 'Athletisme'
+            ],
+            [
+                'name' => 'Michael Phelps',
+                'age' => 35,
+                'category' => 'Natation'
+            ]
+        ];
     }
 
     public function index()
@@ -33,8 +63,8 @@ class SpreadsheetPage extends Controller
 
         $writer = new Xlsx($this->spreadsheet);
         $fileName = 'sedera.xlsx';
-        $writer->save($fileName);
-        if (is_file(FCPATH . $fileName))
+        $writer->save('excel/' . $fileName);
+        if (is_file(FCPATH . 'excel/' . $fileName))
         {
             return 'Excel generated';
         }
@@ -61,8 +91,59 @@ class SpreadsheetPage extends Controller
         $spreadhseet = $reader->loadFromString($secondHtmlString, $spreadsheet);
 
         $writer = IOFactory::createWriter($spreadsheet, 'Xls');
-        $writer->save('html.xls');
+        $writer->save('excel/html.xls');
 
         return 'Excel generated';
+    }
+
+    public function custom()
+    {
+        $sheet = $this->spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1','Nom');
+        $sheet->setCellValue('B1','Age');
+        $sheet->setCellValue('C1','Catégorie');
+
+        $i = 2;
+        foreach ($this->persons as $person) {
+            $sheet->setCellValue('A'.$i,$person['name']);
+            $sheet->setCellValue('B'.$i,$person['age']);
+            $sheet->setCellValue('C'.$i,$person['category']);
+            $i++;
+        }
+
+        $sheet->getStyle('A1:S1')->applyFromArray(
+            array(
+                'font' => array(
+                    'name' => 'Arial',
+                    'bold' => true,
+                    'color' => array('rgb' => 'FF0000'),
+                ),
+                'fill' => array(
+                    'type'  => Fill::FILL_SOLID,
+                    'color' => array('rgb' => '000000'),
+                ),
+            )
+        );
+
+        $sheet->getStyle('A1:U'.$i)->getBorders()->applyFromArray(
+            array(
+                'allborders' => array(
+                    'style' => Border::BORDER_THIN,
+                    'color' => array(
+                        'rgb' => '000000'
+                    )
+                )
+            )
+        );
+
+        $writer = new Xlsx($this->spreadsheet);
+        $fileName = 'custom.xlsx';
+        $writer->save('excel/' . $fileName);
+        if (is_file(FCPATH . 'excel/' . $fileName))
+        {
+            return 'Excel generated';
+        }
+
+        return 'Excel error';
     }
 }
